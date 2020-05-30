86e4d41bb0fa6dceabdcac412c03d86ec2fdd59a
Aggosh <bkmzilia@gmail.com>
2020-05-30 17:41:56 +0300

    rating

---
+++ aviacode/main/rating.py
@@ -0,0 +1,22 @@
+def count(p_time, e_time, p_mem, e_mem):
+    reting = 1
+
+    reting_tablle = {
+        1: 'E',
+        2: 'D',
+        3: 'C',
+        4: 'B',
+        5: 'A'
+    }
+
+    if float(p_time) <= e_time:
+        reting += 2
+    elif float(p_time) <= (e_time*0.75):
+        reting += 2
+
+    if float(p_mem) <= e_mem:
+        reting += 2
+    elif float(p_mem) <= (e_mem*0.75):
+        reting += 2
+
+    return reting_tablle[reting]
